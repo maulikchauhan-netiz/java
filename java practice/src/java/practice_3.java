eafc24bd115b019c952ce308231b405aac3b88e1
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-18 18:51:55 +0530

    java code

---
+++ java practice/src/java/practice_3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class practice_3 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int num = sc.nextInt();
+
+        for(int i=0;i<=num;i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
+
