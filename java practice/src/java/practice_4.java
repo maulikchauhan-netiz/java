41e461b7ff0533fba17381251c4a74e1102c9960
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-19 20:33:03 +0530

    java code

--- java practice/src/java/practice_4.java
+++ java practice/src/java/practice_4.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class practice_4 {
     public static void main(String[] args){
-
+    Scanner sc = new Scanner(System.in);
+    int n = (int)(32.7);
+        System.out.println(n);
     }
 }
