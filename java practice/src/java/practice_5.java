3935e3343e30f33cbcf37613e3d7edcfa2bc5daf
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-21 19:19:19 +0530

    NIS code

---
+++ java practice/src/java/practice_5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class practice_5 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your name : ");
+        System.out.println(sc.next());
+
+    }
+}
