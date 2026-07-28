de3d76e0d2b0ab75cfc107620dc0f363d0aeb5c9
maulik chauhan <maulik.rajput0001@gmail.com>
2026-07-28 18:41:48 +0530

    java code

---
+++ java practice/src/java/practice_6.java
@@ -0,0 +1,15 @@
+public class practice_6 {
+    public static void main (String[]args) {
+        double num = 75.7;
+        int myInt = (int) num;
+        System.out.println(num);
+        System.out.println(myInt);
+
+        int num1=30;
+        double myint1 = num1;
+        System.out.println(num1);
+        System.out.println(myint1);
+
+    }
+
+}
